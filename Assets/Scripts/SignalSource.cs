7de584f0a18407ccacbe73755d9fbe5bcfc922f1
Daniel Clipston <daniel_clipston@hotmail.com>
2022-09-14 15:09:51 +0100

    Started the source signal coding
    The current code can simulate 4 different types of signal.
    The script should be attached to a PORT not the SignalSource GameObject. This is so that signal sources can emit multiple signals from multiple ports.
    ALSO - as of this commit, I haven't tested it properly. I'm assuming it works because it makes me happy, and I want it committed if Unity crashes.
    NEXT - will be to write a way for input signals to be checked against an output signal (and make an output component for it)

--- Assets/Scripts/SignalSource.cs
+++ Assets/Scripts/SignalSource.cs
@@ -5,32 +5,155 @@ using UnityEngine;
 public class SignalSource : SignalComp
 {
 
-    int i_signalValue;//Value at the source
+    int i_signalValue;//Value at the source after ActionValues
+
+    float f_timer;//Float value for the timer
+    float f_pulseTimer;//Float for length of pulse time;
+
+    [SerializeField]
     Port p_outputPort;//Port to output as
+
+    enum SignalType {Static, Oscillating, Random, Pulse };//Possible types of signal
+
+    /* ||~~Type Explanations~~||
+     * ~Static~         Signal stays at a constant value
+     * ~Oscillating~    Signal will rise and fall at a constant rate
+     * ~Random~         Signal value will change randomly
+     * ~Pulse~          Signal will sit at one value, then pulse to another periodically
+     * 
+     */
+
+    [SerializeField]
+    SignalType st_signalType;
+
+    /* ||~~Type variables~~||
+     * ~Static~
+     *  ~i_sStaticValue~     Value that static types will sit at
+     *  
+     * ~Oscillating~
+     *  ~i_oMinValue~       Lowest possible value of the waveform
+     *  ~i_oMaxValue~       Largest possible value of the waveform
+     *  ~i_oStepValue~      Difference between values after each step
+     *  ~f_oRestTime~       Time between steps between values
+     *  ~b_oDirection~      Switch for direction of signal - False goes up, true down
+     *  
+     * ~Random~
+     *  ~i_rMinValue~       Lowest possible random value 
+     *  ~i_rMaxValue~       Largest possible random value
+     *  ~f_rRestTime~       Time between changes to value
+     *  
+     * ~Pulse~
+     *  ~i_pRestValue~      Value the signal rests at between pulses
+     *  ~i_pPulseValue~     Value the singal pulses to
+     *  ~i_pRestTime~       Time between pulses
+     *  ~i_pPulseTime~      Time pulses last
+     *  
+     */
+
+    // ||~~Static~~||
+    [SerializeField]
+    int i_sStaticValue;
+
+    // ||~Oscillating~~||
+    [SerializeField]
+    int i_oMinValue;
+    [SerializeField]
+    int i_oMaxValue;
+    [SerializeField]
+    int i_oStepValue;
+    [SerializeField]
+    float f_oRestTime;
+    bool b_oDirection;
+
+    // ||~~Random~~|
+    [SerializeField]
+    int i_rMinValue;
+    [SerializeField]
+    int i_rMaxValue;
+    [SerializeField]
+    float f_rRestTime;
+
+    // ||~~Pulse~~||
+    [SerializeField]
+    int i_pRestValue;
+    [SerializeField]
+    int i_pPulseValue;
+    [SerializeField]
+    float f_pRestTime;
+    [SerializeField]
+    float f_pPulseTime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        i_signalValue = 0;
+        f_timer = 0;
+        f_pulseTimer = 0;
     }
 
     public override void GetInputs()
     {
-        throw new System.NotImplementedException();
+        //Nothing to do - we are the inputs
     }
 
     public override void ActionValues()
     {
-        throw new System.NotImplementedException();
+        switch (st_signalType)
+        {
+            case SignalType.Static:
+                i_signalValue = i_sStaticValue;
+                //Debug.Log("Static Source: " + i_signalValue);
+                break;
+            case SignalType.Oscillating:
+                f_timer += Time.deltaTime;
+                if (f_timer >= f_oRestTime)
+                {
+                    b_oDirection = !b_oDirection;
+                    f_timer = 0;
+                }
+                if (b_oDirection)
+                {
+                    i_signalValue += i_oStepValue;
+                }
+                else
+                {
+                    i_signalValue -= i_oStepValue;
+                }
+                if (i_signalValue < i_oMinValue) i_signalValue = i_oMinValue;
+                if (i_signalValue > i_oMaxValue) i_signalValue = i_oMaxValue;
+                //Debug.Log("Oscillating Source: " + i_signalValue);
+                break;
+            case SignalType.Random:
+                f_timer += Time.deltaTime;
+                if (f_timer >= f_rRestTime)
+                {
+                    i_signalValue = Random.Range(i_rMinValue, i_rMaxValue);
+                    f_timer = 0;
+                }
+                //Debug.Log("Random Source: " + i_signalValue);
+                break;
+            case SignalType.Pulse:
+                f_timer += Time.deltaTime;
+                if (f_timer >= f_pRestTime || f_pulseTimer != 0)
+                {
+                    i_signalValue = i_pPulseValue;
+                    f_timer = 0;
+                    f_pulseTimer += Time.deltaTime;
+                    if (f_pulseTimer >= f_pPulseTime) f_pulseTimer = 0;
+                }
+                else
+                {
+                    i_signalValue = i_pRestValue;
+                }
+                //Debug.Log("Pulse Source: " + i_signalValue);
+                break;
+            default:
+                break;
+        }
     }
 
     public override void SetOutputs()
     {
-        throw new System.NotImplementedException();
+        p_outputPort.SetConnectedValue(i_signalValue);
     }
 }

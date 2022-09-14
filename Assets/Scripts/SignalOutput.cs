fc910e5b8ea47a356319b51ac06f255c6f05c035
Daniel Clipston <daniel_clipston@hotmail.com>
2022-09-14 16:31:04 +0100

    SignalInput and SignalOutput has been started
    These would be the pieces that hold the information that would allow the SignalOutput to check if the values reaching it are correct in the context of a puzzle.
    I haven't done much, and I might start again. I need to think about how the checks will be performed in a compartmentalised way.
    It might be that a script will create and instantiate the pairs, hold a ref to both, and check the outputs.
    It might be that the pairs just hold different numbers of Inputs/Outputs and use an external script to check them based on which ever "puzzle" has been selected.
    
    I need to think

---
+++ Assets/Scripts/SignalOutput.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SignalOutput : MonoBehaviour
+{
+
+    [SerializeField]
+    SignalInput si_signalInputPair;//Component providing the signals
+
+
+    int i_numberOfSourceValues;//Number of source values at any given time - taken from source pair
+    [SerializeField]
+    public int i_numberofOutputValues;//Number of output values at any given time - independant of source pair
+
+
+    [SerializeField]
+    int i_numberOfInputs;//Number of input ports - serialized as it will differ between instances
+    int i_numberOfOutputs;//Number of output ports - should always be 1
+
+    [SerializeField]
+    List<Port> l_inputPorts;//List of input Ports - can change between instances
+    List<Port> l_outputPorts;//List of output ports
+
+    [SerializeField]
+    Port p_outputPort;//The output port - should connect to a GameObject(Either empty, or antenna or smth) that will check if this port is true or false
+    
+    void Start()
+    {
+        i_numberOfSourceValues = si_signalInputPair.i_numberofSourceValues;
+
+        i_numberOfOutputs = 1;
+        l_outputPorts = new List<Port>(i_numberOfOutputs);
+        l_outputPorts.Add(p_outputPort);
+
+    }
+    
+    void Update()
+    {
+
+    }
+}
